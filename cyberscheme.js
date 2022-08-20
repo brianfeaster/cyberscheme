bfa11f00aa99af388549ee65fac4b7973ad51140
Brian Feaster <brianf@gmail.com>
2022-08-20 18:48:15 +0000

    Unified webworker

--- cyberscheme.js
+++ cyberscheme.js
@@ -1,7 +1,150 @@
 "use strict";
 
-Object.prototype.str = function () { return str(this); };
-Array.prototype.isEmpty = function () { return 0==this.length; };
+////////////////////////////////////////////////////////////////////////////////
+// Scanner
+
+function makeStatesTable (desc) {
+  let states = Array.from(new Array(257), (_)=>desc[0]); // table initialied with default state
+  for (let i=1; (i < desc.length); i+=2) {
+    desc[i]
+      .split("")
+      .forEach( (c) => states[c.charCodeAt()] = desc[i+1] );
+  }
+  return states;
+}
+
+function makeStateMachine (...args) {
+  let table = [];
+  for (let i=0; (i < args.length); i+=2) {
+    table[args[i]] = makeStatesTable(args[i+1]);
+  }
+  return table;
+}
+
+// Intermediate states
+let Start=0, Comment=1, White=2, Dot=3, Dash=4, Floatsym=5, Num=6, Float=7, Sym=8, Str=9, Hash=10;
+// Final states
+let QUOTE=201, OPEN=202, CLOSE=203, STR=204, FALSE=205, TRUE=206, EOF=998, ERROR=999;
+// Final states requiring unget
+let DOT=1002, NUM=1003, FLOAT=1004, SYM=1005, COMMENT=1098, WHITE=1099;
+
+let fsm = makeStateMachine(
+  Start, [ERROR,
+          ";",Comment, "\b\t\n\v\f\r ",White, ".",Dot, "-",Dash, "0123456789",Num, "'",QUOTE, "([{",OPEN, ")]}",CLOSE,
+          "\"",Str, "!$%@*+/:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym, "#",Hash,
+          "\u0100",EOF],
+  Comment, [Comment, "\n\u0100",COMMENT],
+  White,   [WHITE, "\b\t\n\v\f\r ",White],
+  Dot,     [DOT, "0123456789",Float,             "!$%@*+-./:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym],
+  Dash,    [SYM, ".",Floatsym, "0123456789",Num,  "!$%@*+-/:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym],
+  Floatsym,[SYM, "0123456789",Float,             "!$%@*+-./:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym],
+  Num,     [NUM, "0123456789",Num, ".",Float,     "!$%@*+-/:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym],
+  Float,   [FLOAT, "0123456789",Float,           "!$%@*+-./:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym],
+  Sym,     [SYM,                       "!$%@*+-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_abcdefghijklmnopqrstuvwxyz|~",Sym],
+  Str,     [Str, "\"\u0100",STR],
+  Hash,    [Str, "fF",FALSE,  "tT",TRUE]
+);
+
+let text="", start=0, ptr=0, state=Start;
+function init (txt) { text=txt;  start = ptr = state = 0; };
+function getc () { return text.length<=ptr ? 256 : text.charCodeAt(ptr++); }
+function ungetc () { 0<ptr && ptr--;  }
+function isFinalState () { return 100 <= state; }
+function isFinalUngetState () { return 1000 <= state; }
+function isUseableToken () { return (state%100) < 50; }
+function token () {
+  return [
+    state,
+    state==EOF ? "#eof" : text.substr(start, ptr-start)
+  ];
+};
+function reset () { state=Start; start=ptr; }
+function eof () { return state == EOF; }
+function scanNextToken () {
+  isFinalState() && reset(); // Reset state machine if called from a final state.
+  let c;
+  while (!isFinalState()) { state = fsm[state][c=getc()]; }
+  if ( isFinalUngetState() && c!=256 ) { ungetc(); } // Maybe unget last char but only if not EOF
+  return token();
+}
+
+function scanner (scm) {
+  init(scm);
+  let tokens = [];
+  while (!eof()) { tokens.push(...scanNextToken()); }
+  return tokens;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Parser
+
+const newPair = (a,b)=>{ return {car:a, cdr:b}; };
+
+//let QUOTE=201, OPEN=202, CLOSE=203, STR=204, FALSE=205, TRUE=206, EOF=998, ERROR=999;
+//let DOT=1002, NUM=1003, FLOAT=1004, SYM=1005, COMMENT=1098, WHITE=1099;
+
+function parse (tokens) {
+  if (tokens.length < 2) { return null; }
+  switch (tokens[0]) { case WHITE : case COMMENT : tokens.shift(); tokens.shift(); return parse(tokens); }
+  let tok = tokens.shift();
+  let txt = tokens.shift()
+  switch (tok) {
+    case OPEN : return parseList(tokens);
+    case CLOSE : return null;
+    case QUOTE : return newPair("quote", newPair(parse(tokens), null));
+    case NUM :
+    case FLOAT : return parseFloat(txt);
+    case FALSE : return false;
+    case TRUE : return true;
+    case EOF : return undefined;
+    default : return txt;
+  }
+}
+
+function parseList (tokens) {
+  if (tokens.length < 2) { return null; }
+  switch (tokens[0]) {
+    case WHITE :
+    case COMMENT :
+      tokens.shift();
+      tokens.shift();
+      return parseList(tokens);
+  }
+  let tok = tokens.shift();
+  let txt = tokens.shift();
+  switch (tok) {
+    case OPEN : return newPair(parseList(tokens), parseList(tokens));
+    case CLOSE : return null;
+    case EOF: return null;
+    case DOT : let ret = parse(tokens);
+               if (ret === undefined) { ret = null; }
+               if (tokens[0]==CLOSE) {
+                 tokens.shift(); // TODO assume/consume CLOSE
+                 tokens.shift();
+               }
+               return ret;
+    case QUOTE : return newPair(newPair("quote", newPair(parse(tokens), null)), parseList(tokens));
+    default : tokens.unshift(txt);
+              tokens.unshift(tok);
+              let a = parse(tokens);
+              return newPair(a, parseList(tokens));
+  }
+}
+
+function parser (tokens) {
+  let result = [];
+  while (0 < tokens.length) {
+    let e = parse(tokens);
+    undefined !== e && result.push(e);
+  }
+  return result;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Main
+
 
 ////////////////////////////////////////
 // IPC
@@ -136,12 +279,11 @@ let lastEnv = null; // Set throughout evaluation for debugging purposes
 
 function transmogrify (e, cont) {
   let r = transmogrify_(e, cont);
-  //log.bind("TRANSPILE:")(r, "->", cont, e);
   return r;
 }
 
 function transmogrifyBlock (ary, cont) {
-  if (ary.isEmpty()) {
+  if (0 == ary.length) {
     cont = transmogrify(null, cont);
   } else {
     cont = transmogrify(ary.pop(), cont);
@@ -153,7 +295,6 @@ function transmogrifyBlock (ary, cont) {
           return cont.bind(this);
       })(cont));
   };
-  //log.bind("TRANSPILE:")(cont, ary);
   return cont;
 }
 
@@ -437,11 +578,11 @@ function transmogrify_ (e, cont) {
 
 
 // Compile expression into a continuation
-function compile (sexprs) {
+function compile (sexpers) {
   let cont;
   try {
      cont =
-       transmogrifyBlock(sexprs, function REPL_END(){return false;})
+       transmogrifyBlock(sexpers, function REPL_END(){return false;})
          .bind(lastEnv=tge);
   } catch(err) {
      log(err);
@@ -484,8 +625,8 @@ function run (cont) {
   try {
     while (cont && max--) {
       //Vt100.NewChild("b", `${cont.name}\n`).AddClass('db').AddColor("darkmagenta");
-      cont = cont();
       //log("VM:", cont.name, "\n", lastEnv)
+      cont = cont();
       //log(null, lastEnv);
     }
   } catch(e) {
@@ -508,14 +649,14 @@ function run (cont) {
 
 let vm = (()=>{
   let scheduled = false;
-  let continuation = false;
+  let program = false;
   let brk = false;
 
   let exec = function () {
     scheduled = false;
-    if (!brk && continuation) {
-      continuation = run(continuation);
-      if (!continuation) {
+    if (!brk && program) {
+      program = run(program);
+      if (!program) {
         log("CyberScheme WebWorker done.");
       } else {
         scheduled = true;
@@ -527,13 +668,13 @@ let vm = (()=>{
   let self = {};
 
   self.run = function (prog) {
-    continuation = prog;
+    program = prog;
     brk = false;
     scheduled || exec();
   };
 
   self.brk = function () {
-    brk = continuation && !brk;
+    brk = program && !brk;
     scheduled || exec();
   };
 
@@ -542,35 +683,16 @@ let vm = (()=>{
 
 ////////////////////////////////////////
 
-let scanner, parser;
-
-onmessage = function (m) {
-  const newWorker = (code)=>new Worker(URL.createObjectURL(new Blob([code], {type: "text/javascript"})));
-  scanner = newWorker(m.data);
-  onmessage = function (m) {
-    parser = newWorker(m.data);
-    onmessage = dispatch;
-  };
-};
-
-function dispatch (msg) {
+onmessage = function (msg) {
   msg = msg.data;
   //log(`CyberScheme WebWorker ${msg.version} ${msg.type} ${msg.data && msg.data.length}`);
   switch (msg.type) {
   case 1:
     let scm = msg.data;
-    scanner.onmessage = function (tokens) {
-      tokens = tokens.data;
-      //dumpRawTokens(tokens)
-      //console.log(tokens);
-      parser.onmessage = function (sexpers) {
-        sexpers = sexpers.data
-        //console.log(sexpers.str());
-        vm.run(compile(sexpers));
-      };
-      parser.postMessage(tokens);
-    };
-    scanner.postMessage(scm);
+    let tokens = scanner(scm);
+    let sexpers = parser(tokens);
+    let prog = compile(sexpers);
+    vm.run(prog);
     break;
   case 2:
     vm.brk();
